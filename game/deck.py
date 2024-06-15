17a2a54a3e75f0e804f43c2adf7f5f129dc7795c
Omer <omer.holtzman@gmail.com>
2024-06-15 13:48:17 +0300

    deck now hide cards

--- game/deck.py
+++ game/deck.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
+from copy import deepcopy
 from random import shuffle
-from typing import List, Dict
+from typing import List, Dict, Counter
 
 from game.card import Card, Suit
 
@@ -7,33 +9,73 @@ SUITS = [Suit.CLUBS, Suit.DIAMONDS, Suit.HEARTS, Suit.SPADES]
 
 
 class Deck:
-    def __init__(self):
-        self.cards_left: List[Card] = [Card(suit, number) for suit in SUITS for number in range(1, 14)]
-        self.shuffle()
+    def __init__(self, initial_cards: List[Card] = None):
+        if initial_cards is None:
+            self._cards_left: List[Card] = [Card(suit, number) for suit in SUITS for number in range(1, 14)]
+            self.shuffle()
 
-        self.cards_left_amount = len(self.cards_left)
-        self.suits_left: Dict[Suit, int] = {suit: 13 for suit in SUITS}
-        self.number_left: Dict[int, int] = {number: 4 for number in range(1, 14)}
+            self.cards_left_amount = len(self._cards_left)
+            self._suits_left: Dict[Suit, int] = {suit: 13 for suit in SUITS}
+            self._number_left: Dict[int, int] = {number: 4 for number in range(1, 14)}
+            self._decks: List["Deck"] = []
+
+        else:
+            self._cards_left: List[Card] = initial_cards
+            self.cards_left_amount = len(self._cards_left)
+            self._suits_left: Dict[Suit, int] = defaultdict(int)
+            self._number_left: Dict[int, int] = defaultdict(int)
+
+            for card in self._cards_left:
+                self._suits_left[card.suit] += 1
+                self._number_left[card.number] += 1
 
     def pop(self) -> Card | None:
-        if self.cards_left_amount:
-            returned_card = self.cards_left.pop()
-        else:
+        if self.cards_left_amount == 0:
             return None
 
+        returned_card = self._cards_left.pop()
         self.cards_left_amount -= 1
-        self.suits_left[returned_card.suit] -= 1
-        self.number_left[returned_card.number] -= 1
+        self._suits_left[returned_card.suit] -= 1
+        self._number_left[returned_card.number] -= 1
+
+        if self.cards_left_amount == 12:
+            self._decks = [Deck(deepcopy(self._cards_left)), Deck(deepcopy(self._cards_left))]
+
+        if self.cards_left_amount < 12:
+            deck_index_to_remove = (self.cards_left_amount + 1) % 2
+            self._decks[deck_index_to_remove].remove_card(returned_card)
 
         return returned_card
 
+    def remove_card(self, card: Card) -> None:
+        if card in self._cards_left:
+            self._cards_left.remove(card)
+            self._number_left[card.number] -= 1
+            self._suits_left[card.suit] -= 1
+            self.cards_left_amount -= 1
+
     def shuffle(self) -> None:
-        shuffle(self.cards_left)
+        shuffle(self._cards_left)
 
-    def get_suit_left(self, suit: Suit) -> int:
-        return self.suits_left[suit]
+    def get_suit_left(self, suit: Suit, player: bool) -> int:
+        if self.cards_left_amount > 12:
+            return self._suits_left[suit]
+        else:
+            player_deck_index = 0 if player else 1
+            return self._decks[player_deck_index]._suits_left[suit]
 
-    def get_number_left(self, number: int) -> int:
+    def get_number_left(self, number: int, player: bool) -> int:
         if number == 14:
-            return self.number_left[1]
-        return self.number_left[number]
+            number = 1
+
+        if self.cards_left_amount > 12:
+            return self._number_left[number]
+        else:
+            player_deck_index = 0 if player else 1
+            return self._decks[player_deck_index]._number_left[number]
+
+    def get_cards_left(self, player: bool) -> List[Card]:
+        if self.cards_left_amount < 12:
+            player_deck_index = 0 if player else 1
+            return self._decks[player_deck_index]._cards_left
+        return self._cards_left
--- tests/test_deck.py
+++ tests/test_deck.py
@@ -4,20 +4,22 @@ from game.card import Suit
 from game.deck import Deck
 
 
-class TestDeck(unittest.TestCase):
-    def test_deck_sanity(self):
+class TestDeckSanity(unittest.TestCase):
+    def test_deck_initialization(self):
         deck = Deck()
         self.assertEqual(deck.cards_left_amount, 52)
-        self.assertEqual(deck.get_suit_left(Suit.DIAMONDS), 13)
-        self.assertEqual(deck.get_suit_left(Suit.DIAMONDS), deck.get_suit_left(Suit.SPADES))
+        for suit in Suit.__members__.values():
+            self.assertEqual(deck.get_suit_left(suit, True), 13)
+        for number in range(1, 14):
+            self.assertEqual(deck.get_number_left(number, True), 4)
 
     def test_pop_card(self):
         deck = Deck()
         card = deck.pop()
         self.assertIsNotNone(card)
         self.assertEqual(deck.cards_left_amount, 51)
-        self.assertEqual(deck.get_suit_left(card.suit), 12)
-        self.assertEqual(deck.get_number_left(card.number), 3)
+        self.assertEqual(deck.get_suit_left(card.suit, True), 12)
+        self.assertEqual(deck.get_number_left(card.number, True), 3)
 
     def test_pop_two_cards(self):
         deck = Deck()
@@ -28,16 +30,16 @@ class TestDeck(unittest.TestCase):
 
         self.assertEqual(deck.cards_left_amount, 50)
         if first_card.suit == second_card.suit:
-            self.assertEqual(deck.get_suit_left(first_card.suit), 11)
+            self.assertEqual(deck.get_suit_left(first_card.suit, True), 11)
         else:
-            self.assertEqual(deck.get_suit_left(first_card.suit), 12)
-            self.assertEqual(deck.get_suit_left(second_card.suit), 12)
+            self.assertEqual(deck.get_suit_left(first_card.suit, True), 12)
+            self.assertEqual(deck.get_suit_left(second_card.suit, True), 12)
 
         if first_card.number == second_card.number:
-            self.assertEqual(deck.get_number_left(first_card.number), 2)
+            self.assertEqual(deck.get_number_left(first_card.number, True), 2)
         else:
-            self.assertEqual(deck.get_number_left(first_card.number), 3)
-            self.assertEqual(deck.get_number_left(second_card.number), 3)
+            self.assertEqual(deck.get_number_left(first_card.number, True), 3)
+            self.assertEqual(deck.get_number_left(second_card.number, True), 3)
 
     def test_pop_cards(self):
         deck = Deck()
@@ -52,9 +54,56 @@ class TestDeck(unittest.TestCase):
         self.assertIsNone(deck.pop())
         self.assertEqual(deck.cards_left_amount, 0)
         for suit in Suit.__members__.values():
-            self.assertEqual(deck.get_suit_left(suit), 0)
+            self.assertEqual(deck._suits_left[suit], 0)
         for card_number in range(1, 14):
-            self.assertEqual(deck.get_number_left(card_number), 0)
+            self.assertEqual(deck._number_left[card_number], 0)
+
+
+class TestDeckHiddenCards(unittest.TestCase):
+    def test_deck_first_hidden_pop_card(self):
+        deck = Deck()
+        for _ in range(40):
+            deck.pop()
+        first_player_card = deck.pop()
+
+        self.assertEqual(deck.cards_left_amount, 11)
+
+        self.assertFalse(first_player_card in deck.get_cards_left(True))
+        self.assertTrue(first_player_card in deck.get_cards_left(False))
+
+        self.assertEqual(deck.get_suit_left(first_player_card.suit, True) + 1,
+                         deck.get_suit_left(first_player_card.suit, False))
+
+        self.assertEqual(deck.get_number_left(first_player_card.number, True) + 1,
+                         deck.get_number_left(first_player_card.number, False))
+
+    def test_pop_multiple_hidden_cards(self):
+        players_cards = [[], []]
+
+        deck = Deck()
+        for _ in range(40):
+            deck.pop()
+
+        for i in range(12):
+            players_cards[i % 2].append(deck.pop())
+
+        self.assertEqual(deck.cards_left_amount, 0)
+        self.assertNotEqual(players_cards[0], players_cards[1])
+        self.assertListEqual(sorted(players_cards[0]), sorted(deck.get_cards_left(False)))
+        self.assertListEqual(sorted(players_cards[1]), sorted(deck.get_cards_left(True)))
+
+        first_suits = [card.suit for card in players_cards[0]]
+        second_suits = [card.suit for card in players_cards[1]]
+        first_numbers = [card.number for card in players_cards[0]]
+        second_numbers = [card.number for card in players_cards[1]]
+
+        for suit in Suit.__members__.values():
+            self.assertEqual(deck.get_suit_left(suit, True), second_suits.count(suit))
+            self.assertEqual(deck.get_suit_left(suit, False), first_suits.count(suit))
+
+        for number in range(1, 14):
+            self.assertEqual(deck.get_number_left(number, True), second_numbers.count(number))
+            self.assertEqual(deck.get_number_left(number, False), first_numbers.count(number))
 
 
 if __name__ == '__main__':
